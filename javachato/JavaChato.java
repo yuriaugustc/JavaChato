3f7189c62992680c1de5ec56ba5f471f94f6a97c
Yuri Augusto da Costa <yuri.costa@sedoc.local>
2022-05-31 17:31:30 -0300

    inicializando o projeto

---
+++ javachato/JavaChato.java
@@ -0,0 +1,12 @@
+package javachato;
+
+/**
+ * @author yuriaugustc
+ */
+public class JavaChato {
+
+    public static void main(String[] args) {
+        System.out.println("Projeto JavaChato!");
+    }
+    
+}
